a5f0dd69936c1db15266936c912c1a5f1302493d
maria.khmaruk@gmail.com <maria.khmaruk@gmail.com>
2020-02-21 00:48:53 +0200

    Revert "Revert "Rozetka Task added""
    This reverts commit 64e1ca4111120b63515de774d83aab8d0a8d9de1.

--- NUnitSelenium1Homework/UnitTest2.cs
+++ NUnitSelenium1Homework/UnitTest2.cs
@@ -22,7 +22,7 @@ namespace NUnitSelenium1Homework
         public void Setup()
         {
             _driver = new ChromeDriver();
-            _driver.Manage().Timeouts().ImplicitWait = new TimeSpan(0, 0, 7);
+            _driver.Manage().Timeouts().ImplicitWait = new TimeSpan(0, 0, 5);
             _driver.Manage().Window.Maximize();
             _driver.Navigate().GoToUrl("https://en.wikipedia.org/wiki/Main_Page");
         }
@@ -30,12 +30,30 @@ namespace NUnitSelenium1Homework
         [Test]
         public void WikipediaPictures()
         {
-            //var images = _driver.FindElements(By.TagName("img"));
+            var images = _driver.FindElements(By.TagName("img"));
+            for (int i = 1; i <= images.Count; i++)
+            {
+                if (images[i-1].Size.Width<100)
+                {
+                    continue;
+                }
 
-            var ele = By.TagName("img");
-            var arr = _driver.TakeScreenshot(new OnlyElementDecorator(new ScreenshotMaker()).SetElement(ele));
-            string path = Directory.GetCurrentDirectory();
-            File.WriteAllBytes(path, arr);
+                var ele = By.XPath($"(//img)[{i}]");
+                var arr = _driver.TakeScreenshot(new OnlyElementDecorator(new ScreenshotMaker()).SetElement(ele));
+                File.WriteAllBytes(@$"C:\Users\Masha\Desktop\images scrshts\scr{i}.png", arr);
+            }            
         }
+
+        //[Test]
+        //public void WikipediaPicturesTest()
+        //{
+        //    var images = _driver.FindElements(By.XPath("//*[@class='image']"));
+        //    for (int i = 1; i <= images.Count; i++)
+        //    {
+        //        var ele = By.XPath("//*[@class='image']");
+        //        var arr = _driver.TakeScreenshot(new OnlyElementDecorator(new ScreenshotMaker()).SetElement(ele));
+        //        File.WriteAllBytes(@$"C:\Users\Masha\Desktop\images scrshts\scr{i}.png", arr);
+        //    }
+        //}
     }
 }
